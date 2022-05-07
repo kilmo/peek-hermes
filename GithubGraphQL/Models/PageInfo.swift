0abd062198704838693b44f632fce5fc8f5d661d
moriya <erikamoriya@gmail.com>
2022-05-07 17:07:35 -0300

    feat: adding models and groups for mvvm

--- GithubGraphQL/Models/PageInfo.swift
+++ GithubGraphQL/Models/PageInfo.swift
@@ -1,9 +1,7 @@
-//
-//  PageInfo.swift
-//  GithubGraphQL
-//
-//  Created by Érika Moriya on 07/05/22.
-//  Copyright © 2022 test. All rights reserved.
-//
-
-import Foundation
+struct PageInfo: Decodable {
+    let pageInfo: String
+    let hasNextPage: Bool
+    let hasPreviousPage: Bool
+    let startCursor: String?
+    let endCursor: String?
+}
--- GithubGraphQL/Models/Repository.swift
+++ GithubGraphQL/Models/Repository.swift
@@ -1,9 +1,15 @@
-//
-//  Repository.swift
-//  GithubGraphQL
-//
-//  Created by Érika Moriya on 07/05/22.
-//  Copyright © 2022 test. All rights reserved.
-//
-
-import Foundation
+struct Repository: Decodable {
+    let name: String
+    let path: String
+    let owner: String
+    let avatar: String
+    let stars: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case name = "Name"
+        case path = "Path"
+        case owner = "Owner"
+        case avatar
+        case stars = "Stars"
+    }
+}
