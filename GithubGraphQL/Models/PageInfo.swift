1e3b4c796f2421c1af0ce37210874e82b62e03bb
moriya <erikamoriya@gmail.com>
2022-05-07 17:06:47 -0300

    feat: adding git key

---
+++ GithubGraphQL/Models/PageInfo.swift
@@ -0,0 +1,9 @@
+//
+//  PageInfo.swift
+//  GithubGraphQL
+//
+//  Created by Érika Moriya on 07/05/22.
+//  Copyright © 2022 test. All rights reserved.
+//
+
+import Foundation
---
+++ GithubGraphQL/Models/Repository.swift
@@ -0,0 +1,9 @@
+//
+//  Repository.swift
+//  GithubGraphQL
+//
+//  Created by Érika Moriya on 07/05/22.
+//  Copyright © 2022 test. All rights reserved.
+//
+
+import Foundation
